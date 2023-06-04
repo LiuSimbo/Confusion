6ae07248978d64d2d9021741a079e04fd38cf594
Anh Quan <quantade160213@fpt.edu.vn>
2023-06-04 18:47:04 +0700

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
-import logo from './logo.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import React, { Component } from 'react';
-import Menu from './components/MenuConponent';
+import "./App.css";
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, NavbarBrand } from "reactstrap";
+import React, { Component } from "react";
+import Menu from "./components/MenuConponent";
+import { DISHES } from "./shared/dishes";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,12 +22,10 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
 }
 
-
 export default App;
-
